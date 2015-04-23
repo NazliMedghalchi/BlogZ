6c37a03cf00ff3dd1cc57e5159eeb2615ba6bcbf
Nazli Medghalchi <nmedghal@uwaterloo.ca>
2015-04-23 19:51:45 -0400

    Listen 3030

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ app.use(stylus.middleware(
 	}
 ))
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/views'))
 
 app.get('/', function (req, res) {
 	res.render ('index')
 })
-app.listen (3030)
+app.listen(3030)

0d1aa037233b507a3676f2263d83aa560c87e069
Nazli Medghalchi <nmedghal@uwaterloo.ca>
2015-04-23 13:22:07 -0400

    Fixed app.js -- runnign on 3030

--- app.js
+++ app.js
@@ -4,23 +4,25 @@
 var express = require ('express')
 		, stylus = require ('stylus')
 		, nib = require ('nib')
+		, path = require('path')
 var app = express()
 function compile (str, path){
 	return stylus (str)
 			. set ('filename', path)
 			. use (nib())
 }
-app.set('views', __dirname + '/views');
-app.set('view engin', 'jade');
+app.set('views', __dirname  +'/views');
+app.set('view engine', 'jade');
 app.use(express.logger('dev'));
-app.use(styllus.middleware(
-{		src: __dirname + '/public'
+app.use(stylus.middleware(
+	{	src: __dirname + '/public'
 	,	compile : compile
-}
-));
+	}
+))
 
-app.use(express.static(__dirnanme + '/public'))
+app.use(express.static(__dirname + '/public'))
+
 app.get('/', function (req, res) {
-	res.end ('Hi there!')
-});
-app.listen (3000);
+	res.render ('index')
+})
+app.listen (3030)
